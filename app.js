ee36e19f98301bd577e03558ad0542cd25240ba9
riyadh <riyadh02220@gmail.com>
2019-05-08 16:34:49 +0200

    show the favorite music when choising music from hobbites

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 // DOM ELEMENT REFERENCES
 const hobbies = document.getElementById('hobbies');
+const music = document.getElementById('music');
 const transport = document.getElementById('transport');
 const select = document.getElementById('music-preference');
 
 const hobbiesResult = document.getElementById('hobbies-result');
 const transportResult = document.getElementById('transport-result');
 const musicResult = document.getElementById('music-result');
+const shoxMusic = document.getElementById('shoxMusic');
+
+musicResult.textContent = select.value;
 
 // CHECKBOX LISTENERS :
 function checkbox(n) {
     hobbies.childNodes[n].childNodes[1].addEventListener("change",(event)=> {
         event.target.checked?hobbiesResult.childNodes[n].classList.remove("text-secondary"):
         hobbiesResult.childNodes[n].classList.add("text-secondary");
+        if(n === 5 && event.target.checked) {
+            music.style.display = "block";
+            shoxMusic.style.display = "block";
+        }else if(!event.target.checked) {
+            music.style.display = "none";
+            shoxMusic.style.display = "none";
+        }
     })
 }
 checkbox(1); checkbox(3); checkbox(5);
